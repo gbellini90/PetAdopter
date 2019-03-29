494496af397cd4cf313b18cb6c42ae9e05fbaef1
gbellini90 <gbellini90@gmail.com>
2019-03-29 11:40:22 -0400

    Add more seed data

--- PetAdopter/db/seeds.rb
+++ PetAdopter/db/seeds.rb
@@ -26,6 +26,7 @@ parsed_data_sfth["petfinder"]["pets"]["pet"].each do |pet|
   else
     breed = pet["breeds"]["breed"]["$t"]
   end
+
     photo = pet["media"]["photos"]["photo"].find { |photo| photo["@size"]=="x"}["$t"]
   Pet.create!(name: pet["name"]["$t"], age: pet["age"]["$t"], size: pet["size"]["$t"], sex: pet["sex"]["$t"], description: pet["description"]["$t"], animal: pet["animal"]["$t"], breed: breed, shelterId:pet["shelterId"]["$t"],photo:photo)
 end
@@ -47,21 +48,50 @@ parsed_data_bideawee["petfinder"]["pets"]["pet"].each do |pet|
   Pet.create!(name: pet["name"]["$t"], age: pet["age"]["$t"], size: pet["size"]["$t"], sex: pet["sex"]["$t"], description: pet["description"]["$t"], animal: pet["animal"]["$t"], breed: breed, shelterId:pet["shelterId"]["$t"],photo:photo)
 end
 
+# Waggytail Rescue data
+waggytail = RestClient.get("http://api.petfinder.com/shelter.getPets?key=#{API_KEY}&id=NY447&count=100&format=json")
+parsed_data_waggytail = JSON.parse(waggytail)
 
-# Brooklyn Animal Action data
-# animalCC = RestClient.get("http://api.petfinder.com/shelter.getPets?key=#{API_KEY}&id=NY947&count=100&count=100&format=json")
-# parsed_data_animalCC = JSON.parse(animalCC)
-#
-# parsed_data_animalCC["petfinder"]["pets"]["pet"].each do |pet|
-#   next if !pet["media"]["photos"]
-#   if pet["breeds"]["breed"][0]
-#     breed = pet["breeds"]["breed"][0]["$t"]
-#   else
-#     breed = pet["breeds"]["breed"]["$t"]
-#   end
-#     photo = pet["media"]["photos"]["photo"].find { |photo| photo["@size"]=="x"}["$t"]
-#   Pet.create!(name: pet["name"]["$t"], age: pet["age"]["$t"], size: pet["size"]["$t"], sex: pet["sex"]["$t"], description: pet["description"]["$t"], animal: pet["animal"]["$t"], breed: breed, shelterId:pet["shelterId"]["$t"],photo:photo)
-# end
+parsed_data_waggytail["petfinder"]["pets"]["pet"].each do |pet|
+  next if !pet["media"]["photos"]
+  if pet["breeds"]["breed"][0]
+    breed = pet["breeds"]["breed"][0]["$t"]
+  else
+    breed = pet["breeds"]["breed"]["$t"]
+  end
+    photo = pet["media"]["photos"]["photo"].find { |photo| photo["@size"]=="x"}["$t"]
+  Pet.create!(name: pet["name"]["$t"], age: pet["age"]["$t"], size: pet["size"]["$t"], sex: pet["sex"]["$t"], description: pet["description"]["$t"], animal: pet["animal"]["$t"], breed: breed, shelterId:pet["shelterId"]["$t"],photo:photo)
+end
+
+#GiveMeShelter data
+givemeshelter = RestClient.get("http://api.petfinder.com/shelter.getPets?key=#{API_KEY}&id=NY1113&count=100&format=json")
+parsed_data_gms = JSON.parse(givemeshelter)
+
+parsed_data_gms["petfinder"]["pets"]["pet"].each do |pet|
+  next if !pet["media"]["photos"]
+  if pet["breeds"]["breed"][0]
+    breed = pet["breeds"]["breed"][0]["$t"]
+  else
+    breed = pet["breeds"]["breed"]["$t"]
+  end
+    photo = pet["media"]["photos"]["photo"].find { |photo| photo["@size"]=="x"}["$t"]
+  Pet.create!(name: pet["name"]["$t"], age: pet["age"]["$t"], size: pet["size"]["$t"], sex: pet["sex"]["$t"], description: pet["description"]["$t"], animal: pet["animal"]["$t"], breed: breed, shelterId:pet["shelterId"]["$t"],photo:photo)
+end
+
+#New Beginning Animal Rescue data
+newbeginning = RestClient.get("http://api.petfinder.com/shelter.getPets?key=#{API_KEY}&id=NY1121&count=100&format=json")
+parsed_data_nb = JSON.parse(newbeginning)
+
+parsed_data_nb["petfinder"]["pets"]["pet"].each do |pet|
+  next if !pet["media"]["photos"]
+  if pet["breeds"]["breed"][0]
+    breed = pet["breeds"]["breed"][0]["$t"]
+  else
+    breed = pet["breeds"]["breed"]["$t"]
+  end
+    photo = pet["media"]["photos"]["photo"].find { |photo| photo["@size"]=="x"}["$t"]
+  Pet.create!(name: pet["name"]["$t"], age: pet["age"]["$t"], size: pet["size"]["$t"], sex: pet["sex"]["$t"], description: pet["description"]["$t"], animal: pet["animal"]["$t"], breed: breed, shelterId:pet["shelterId"]["$t"],photo:photo)
+end
 
 
 
